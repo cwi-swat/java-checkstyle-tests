33317d54fcdbe74b28a4c9553a0931b0c8b30b0e
Jurgen J. Vinju <jurgen@vinju.org>
2014-08-07 17:01:46 +0200

    added checkstyle runner

---
+++ lib/run.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+java \
+  -Dcheckstyle.importcontrol.file="import-control.xml" \
+  -Dcheckstyle.header.file="./header.txt" \
+  -Dcheckstyle.suppressions.file="suppressions.xml" \
+  -Dcheckstyle.cache.file=./.checkStyleCache \
+  -jar checkstyle-5.7-all.jar \
+  -f xml \
+  -c ./checkstyle_checks.xml \
+  -r ../src
