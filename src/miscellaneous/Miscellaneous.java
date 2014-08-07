2b2b711095e61e9d1b07f3ccc456ec7014a578ee
paulklint <Paul.Klint@cwi.nl>
2014-08-07 23:57:50 +0200

    Added tests

---
+++ src/miscellaneous/Miscellaneous.java
@@ -0,0 +1,14 @@
+package miscellaneous;
+
+public class Miscellaneous {
+	/* TODO: define m */
+	void m1(int n){ }
+	
+	int m2(int n){
+		return n; // TODO: make this more interesting
+	}
+	
+	void main(){
+		
+	}
+}
